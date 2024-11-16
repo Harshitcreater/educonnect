24e8dc81447b2f71e284253d0c2a55cad9a9e8c0
Harshitcreater <S24CSEU1834@bennett.edu.in>
2024-11-17 01:34:29 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,42 +1,41 @@
 from flask import Flask, render_template, request, redirect, url_for
 
-app = Flask(__name__)
+app = Flask(__name__) #used to give access to templete and static files to flask
 
-# Dictionary of users with their username as the key, and a dictionary for password and target page
 users = {
     "apeksha": {"password": "apeksha123", "page": "apeksha"},
     "ashwani": {"password": "ashwani123", "page": "ashwani"},
-    "neeraj":{"password":"neeraj123","page":"neeraj1"}
+    "arjun":{"password":"arjun123","page":"arjun"}
 }
 
-@app.route('/')
+@app.route('/') # define route of the url html file
 def home():
-    return render_template('login.html')
+    return render_template('login.html')#accessing the html file from template folder
 
 @app.route('/login', methods=['POST'])
 def login():
-    # Get username and password from the login form
-    username = request.form['username']
+    
+    username = request.form['username']# to enter username and password 
     password = request.form['password']
 
-    # Check if the username exists and if the password matches for that username
-    if username in users and users[username]['password'] == password:
-        # Redirect to the corresponding page for the logged-in user using the function name (not the file)
-        return redirect(url_for(users[username]["page"]))
+    
+    if username in users and users[username]['password'] == password:# check whether the username is present in users or not if yes then it will check whether the password is correct or not 
+    
+        return redirect(url_for(users[username]["page"]))#if matched then it will take it to the wage page which is linked liked to it.
     else:
-        # If the username or password is incorrect, show an error message
-        return render_template('login.html', error="Invalid username or password!")
+        
+        return render_template('login.html', error="Invalid username or password!")#else remain on that page
 
-@app.route('/apeksha')
-def apeksha():
-    return render_template('apeksha.html')
+@app.route('/apeksha')#define route to display html file
+def apeksha():#define function and run further fuction detail
+    return render_template('apeksha.html')# when particular function called it opens particular html file. 
 
 @app.route('/ashwani')
 def ashwani():
     return render_template('ashwani.html')
-@app.route('/neeraj1')
-def neeraj1():
-    return render_template('neeraj1.html')
+@app.route('/arjun')
+def arjun():
+    return render_template('arjun.html')
 @app.route('/aschedule')
 def aschedule():
     return render_template('aschedule.html')
@@ -53,6 +52,14 @@ def athschedule():
 @app.route('/afschedule')
 def afschedule():
     return render_template('afschedule.html')
+@app.route('/ashwanisirschedule')
+def ashwanisirschedule():
+    return render_template('ashwanisirschedule.html')
+@app.route('/arjunsirschedule')
+def arjunsirschedule():
+    return render_template('arjunsirschedule.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
